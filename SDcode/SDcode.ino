5d116fccf2ca365d74ba2d54c1b41e49500f99e8
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2025-06-21 15:57:25 +1200

    Add files via upload

---
+++ SDcode/SDcode.ino
@@ -0,0 +1,67 @@
+#include <SPI.h>
+#include <SD.h>
+
+
+File fridgeFile;
+
+int lightReading = A0;
+int lightValue;
+bool doorChanged = false;
+String doorState;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  pinMode(lightReading, INPUT);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");  
+}
+
+void loop() {
+  // nothing happens after setup
+  lightValue = analogRead(lightReading);
+
+  if(lightValue > 200 && !doorChanged){   
+    fridgeFile = SD.open("test.txt", FILE_WRITE);
+    doorState = "Open ";
+    String info = doorState + ", " + lightValue;
+    Serial.println(info);
+    writeToFile();
+//    if (fridgeFile) {
+//      fridgeFile.println(info);
+//      fridgeFile.close();
+//    }
+    doorChanged = true;
+  }
+  else if(lightValue < 200 && doorChanged){
+    fridgeFile = SD.open("test.txt", FILE_WRITE);
+    doorState = "Closed ";
+    String info = doorState + ", " + lightValue;
+    Serial.println(info);
+    writeToFile();
+//    if (fridgeFile) {
+//      fridgeFile.println(info);
+//      fridgeFile.close();
+//    }
+    doorChanged = false;
+  }
+}
+
+void writeToFile(){
+  if (fridgeFile) {
+      
+      String info = doorState + ", " + lightValue;
+      fridgeFile.println(info);
+      fridgeFile.close();
+    }
+}
